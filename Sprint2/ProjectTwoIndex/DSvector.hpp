d8e353e1bc563e307c201aa5cff4e72259c30f46
Terry Wang <tcwang@smu.edu>
2018-09-14 17:43:43 -0500

    Vector [] added and tested(not for size)

--- Sprint2/ProjectTwoIndex/DSvector.hpp
+++ Sprint2/ProjectTwoIndex/DSvector.hpp
@@ -14,26 +14,72 @@ private:
     int size; //numElements
     int capacity;
 public:
+    DSvector();
     DSvector(int);
+    DSvector(const DSvector&);
     ~DSvector();
+
+    DSvector& operator= (const DSvector&);
+    T& operator[] (int);
+
     bool isEmpty();
-    T operator[] (int);
     void put_back(T);
-    T at(int);
+    T at(const int);
 
 };
 
 //default constructor
+template<class T>
+DSvector<T>::DSvector()
+{
+    size = 0;
+    capacity = 10; //Default Capacity of 10
+    data = new T[10];
+}
 
-
-//Full Contructor
+//Full Constructor
 template<class T>
 DSvector<T>::DSvector(int initCap)
 {
+    if(initCap < 0)
+    {
+        throw logic_error("Size of vector is below zero");
+    }
     size = 0;
     capacity = initCap;
-    data =  new T[size];
+    data =  new T[capacity];
 }
+
+//Copy Consustructor
+template<class T>
+DSvector<T>::DSvector(const DSvector& arg)
+{
+    this -> size = arg.size;
+    this -> capacity = arg.capacity;
+    data = new T[capacity];
+    for(int x = 0; x < size; x++)
+        this -> data[x] = arg.data[x];
+}
+
+//[] operator
+template<class T>
+T& DSvector<T>::operator[](int loc)
+{
+    return data[loc];
+}
+//Assignment Operator
+template<class T>
+DSvector<T>& DSvector<T>::operator=(const DSvector& arg)
+{
+
+    this -> size = arg.size;
+    this -> capacity = arg.capacity;
+    data = new T[capacity];
+    for(int x = 0; x < size; x++)
+        this -> data[x] = arg.data[x];
+    return *this;
+}
+
 //Deconstructor
 template<class T>
 DSvector<T>::~DSvector()
@@ -48,7 +94,7 @@ DSvector<T>::~DSvector()
 //}
 //Returns element at location
 template<class T>
-T DSvector<T>::at(int loc)
+T DSvector<T>::at(int const loc)
 {
     if(loc > size || loc < 0)
     {
--- Sprint2/ProjectTwoIndex/test.cpp
+++ Sprint2/ProjectTwoIndex/test.cpp
@@ -2,6 +2,7 @@
 #include "catch.hpp"
 #include "DSvector.hpp"
 #include <string>
+#include <iostream>
 
 
 using namespace std;
@@ -10,13 +11,43 @@ TEST_CASE("Testing dsvectrs", "[dsvectors]")
 {
     SECTION("Int Tests")
     {
+        DSvector<int> testDefault;
+//        DSvector<int> sizeBelowZero(-1);
+//        CHECK_THROWS_AS(sizeBelowZero, logic_error);
         DSvector<int> testInt(0);
         DSvector<int> testInt2(5);
+        testInt2[0] = 5;
+        cerr << testInt2[0] << '\n';
+        //DSvector<int> testList = {1,2,3};
+
+    }
+
+    SECTION("DOuble TEsts")
+    {
+        DSvector<double> doublething(1);
+        doublething[0] = 2.4;
+        cerr << doublething[0];
     }
 
     SECTION("String tests")
     {
-        DSvector<string> testString(0);
+        DSvector<string> testString(5);
+        string hi = "hi";
+        testString[0] = hi;
+        cerr << testString[0] << '\n';
+        testString[1] = "2nd";
+        cerr << testString[1];
+
+    }
+
+    SECTION("TEst copy and assignment")
+    {
+        DSvector<string> testString(5);
+//        for(int i = 0; i < 5; i++)
+//        {
+
+//        }
+        DSvector<string> testStringAs = testString;
     }
 
     SECTION("at Tests")
