09b110d72c1d20a7dae877f818763c1414ecaf1e
Terry Wang <tcwang@smu.edu>
2018-10-19 20:20:59 -0500

    Working on insertAt

--- Sprint4/ProjectFour/linkedlist.hpp
+++ Sprint4/ProjectFour/linkedlist.hpp
@@ -26,16 +26,22 @@ class LinkedList{
 public:
     //Constructor for nullptr
     LinkedList():
-        head{nullptr}, tail{nullptr}, currIter{nullptr} {}
+        head{nullptr}, tail{nullptr}, currIter{nullptr},length(0) {}
     ~LinkedList();
     void insertFront(T val);
     void insertBack(T val);
+    void insertAt(int pos, T val);
+    int getSize();
     void print();
+    void clear();
     bool isEmpty() const;
 
 private:
+    void insertHere(int pos, T val);
     void insertBack(Node<T>*);
+    void insertFront(Node<T>*);
     Node<T>* head, *tail, *currIter;
+    int length;
 };
 
 //Destructor
@@ -55,6 +61,50 @@ LinkedList<T>::~LinkedList()
     }
 }
 
+/* Get size return the size/length of the List
+ * @return size the size of the list
+ */
+template<typename T>
+int LinkedList<T>::getSize()
+{
+    return length;
+}
+
+/* The insert front function gets a val and sends it to the
+ * other insertFront function that puts it in the front of a node
+ * @param val the value to put it in
+ */
+template<typename T>
+void LinkedList<T>::insertFront(T val)
+{
+    Node<T>* temp = new Node<T>(val);
+    insertFront(temp);
+}
+
+/* The insertFront function inserts a node into the beginning of the List
+ * @param val wiht node
+ */
+template<typename T>
+void LinkedList<T>::insertFront(Node<T>* val)
+{
+    if(isEmpty())
+    {
+        head = val;
+        tail = head; //Works b/c head == nullptr
+    }
+    else
+    {
+        val->next = head;
+        head->prev = val;
+        head = head->prev;
+    }
+    length++;
+}
+
+/* The insert back function gets a val and sends it to the
+ * other insertBack function that puts it int the end of anode
+ * @param val the value to put it in
+ */
 template<typename T>
 void LinkedList<T>::insertBack(T val)
 {
@@ -63,7 +113,7 @@ void LinkedList<T>::insertBack(T val)
 }
 
 /* The insertBack function inserts a node into the end of the List
- * @param val is the value to put in
+ * @param val wiht node
  */
 template<typename T>
 void LinkedList<T>::insertBack(Node<T>* val)
@@ -79,6 +129,52 @@ void LinkedList<T>::insertBack(Node<T>* val)
         tail->next->prev = tail;
         tail = tail->next;
     }
+    length++;
+}
+
+/* InsertAt inserts a value at a position
+ * @param val the value that wil be added
+ * @param pos the position of the value
+ */
+template<typename T>
+void LinkedList<T>::insertAt(int pos, T val)
+{
+    insertHere(pos,val);
+}
+
+/*InsertHere inserts a value at a position but for real
+ * @param val the value that will be added
+ * @param pos position of vlaue
+ */
+template<typename T>
+void LinkedList<T>::insertHere(int pos, T val)
+{
+    if(pos < 0 || pos > length)
+    {
+        throw std::logic_error("insertHere position is out of bounds");
+    }
+
+    if(head != nullptr)
+    {
+        Node<T>* current = head;
+        if(pos == 0){insertFront(val); return;}
+        while(current->next !=nullptr && pos > 0)
+        {
+            current = current->next;
+            pos--;
+        }
+
+        if(current == tail){insertBack(val); return;}
+
+        Node<T>* temp = new Node<T>(val);
+        temp->prev = current;
+        temp->next = current->next;
+        current->next->prev = temp;
+        current->next = temp;
+        length++;
+    }
+    insertFront(val);
+    length++;
 }
 
 /* The Print methods prints everything out
@@ -90,6 +186,7 @@ void LinkedList<T>::print()
     if(!isEmpty())
     {
         Node<T>* current = head;
+        std::cout << "Starting Printing"; // Remove when done
         while(current != nullptr)
         {
            std::cout << current->data;
@@ -99,6 +196,19 @@ void LinkedList<T>::print()
     else{
         throw std::logic_error("Print but Nothings here");
     }
+}
+
+//Clear function removes all of the nodes in the linked list
+template<typename T>
+void LinkedList<T>::clear()
+{
+    while(head != nullptr)
+    {
+        Node<T>* temp = head->next;
+        delete[] head;
+        head = temp;
+    }
+
 }
 
 //IsEmpty
--- Sprint4/ProjectFour/test.cpp
+++ Sprint4/ProjectFour/test.cpp
@@ -25,12 +25,33 @@ TEST_CASE("LinkedList")
         LinkedList<string> stringTest;
     }
 
-    SECTION("TestinsertBack and Print")
+    SECTION("TestinsertBack and insert frontprint and length#1")
     {
         REQUIRE_THROWS_AS(mainTest.print(), logic_error);
         mainTest.insertBack("Hi");
         mainTest.insertBack("Hi2");
+        //mainTest.print();
+        REQUIRE(mainTest.getSize() == 2);
+        mainTest.insertFront("HI-1");
+        mainTest.insertFront("Hi-2");
+        REQUIRE(mainTest.getSize() == 4);
+    }
+
+
+    SECTION("test InsertAt")
+    {
+        mainTest.insertFront("Hi - Pos Former Pos1");
+        mainTest.insertFront("Hi - Pos 0");
         mainTest.print();
+        cout << '\n';
+        mainTest.insertAt(1, "SUPPOSED TO BE AT POS 1");
+        mainTest.print();
+    }
+
+    SECTION("Clear Test and getSize")
+    {
+        mainTest.clear();
+        REQUIRE_THROWS_AS(mainTest.print(),logic_error);
     }
 }
 
